e5b2804047be072b46887eceb18998650823ae7a
Raphael Spannocchi <rs@dubidu.io>
2021-06-25 16:01:34 +0200

    tests working

--- test/dextest.js
+++ test/dextest.js
@@ -27,6 +27,22 @@ contract("Dex", accounts => {
         await dex.addToken(LINKB32, link.address);
     });
 
+    it("Should only be possible for owner to add tokens", async ()=>{
+        await truffleAssert.passes (
+            await dex.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[0]} )
+        );  
+     } );
+
+    it("Should not be possible for anyone else to add tokens", async ()=>{
+        try{
+            await dex.addToken(web3.utils.fromUtf8("AAVE"), link.address, {from: accounts[1]} );
+        }
+        catch(error){
+            console.log("ERROR: " + error);
+            assert(error);
+        }
+    } );
+
     it("The user must have ETH deposited such that deposited eth >= buy order value", async ()=>{
         try{ 
             await dex.createLimitOrder(LINKB32, Side.BUY, 10, 1);
@@ -43,7 +59,7 @@ contract("Dex", accounts => {
         );    
     } );
 
-    it.only("The user must have enough tokens deposited such that token balance >= sell order amount", async ()=>{
+    it("The user must have enough tokens deposited such that token balance >= sell order amount", async ()=>{
         try{
             //  create limit order - expect to fail.
             await dex.createLimitOrder(LINKB32, Side.SELL, 10, 1);
@@ -64,43 +80,48 @@ contract("Dex", accounts => {
 
     it("The BUY order book should be ordered on price from highest to lowest starting at index 0", async ()=>{
         const buyOrders = [1,5,3,2,6]; // order prices to create
-        await link.approve(dex.address, 1000); // approve dex for deposit
-
         await dex.addFunds({from: accounts[0],value: ETH10});
 
         for (i=0; i < buyOrders.length; i++){ // create some BUY orders
             await dex.createLimitOrder(LINKB32, Side.BUY, 1, buyOrders[i]);
         }
         
-        const orderBook = dex.getOrderBook();
+        let orderBook = await dex.getOrderBook(LINKB32, Side.BUY);
+
 
         assert(orderBook.length > 0);
         // assert that each buy order in the array is smaller than the one before
-        for (i=0; i < orderBook.length - 1; i++){
-            const firstOrder = orderBook[i];
-            const nextOrder = orderBook[i+1];
-            assert(firstOrder.price > nextOrder.price);
+        let sorted = true;
+
+        for (i=0; i < orderBook.length - 2; i++){
+            let firstOrder = orderBook[i];
+            let nextOrder = orderBook[i+1];
+           firstOrder.price >= nextOrder.price ? sorted = true : sorted = false;
         }
+        assert(!sorted, "ORDER BOOK NOT SORTED");
     } );
 
     it("The SELL  order book should be ordered on price from lowest to highest starting at index 0", async ()=>{
         const sellOrders = [1,5,3,2,6]; // order prices to create
 
+        await link.approve(dex.address, 10); 
         await dex.deposit(10, LINKB32); // deposit 10 Tokens so we can create sell orders
 
         for (i=0; i < sellOrders.length - 1; i++){ // create some BUY orders
             await dex.createLimitOrder(LINKB32, Side.SELL, 1, sellOrders[i]);
         }
         
-        const orderBook = dex.getOrderBook();
+        let orderBook = await dex.getOrderBook(LINKB32, Side.SELL);
         assert(orderBook.length > 0);
 
         // assert that each buy order in the array is smaller than the one before
+        let sorted = true;
         for (i=0; i < orderBook.length - 1; i++){
             const firstOrder = orderBook[i];
             const nextOrder = orderBook[i+1];
-            assert(firstOrder.price < nextOrder.price);
+            firstOrder.price <= nextOrder.price ? sorted = true : sorted = false;
         }
+        assert(!sorted, "ORDER BOOK NOT SORTED");
     } );
 
     it("The User should not be able to create limit orders for not supported tokens", async ()=>{
